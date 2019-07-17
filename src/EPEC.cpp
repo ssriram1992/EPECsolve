afc684cd1be6beb8b163a8c3281188d689f62aac
Gabriele Dragotto <6563103+gdragotto@users.noreply.github.com>
2019-07-17 12:01:41 -0400

    Merging "models" and "gabriele"
    Game::MP_Param& Game::MP_Param::addDummy and EPEC.cpp needs to be merged yet. Need to check the NASP with more than 2 countries.

--- src/EPEC.cpp
+++ src/EPEC.cpp
@@ -12,92 +12,103 @@
 using namespace std;
 
 
-int LCPtest(Models::LeadAllPar LA, Models::LeadAllPar LA2, arma::sp_mat TrCo)
-{
-	GRBEnv env = GRBEnv();
-	// GRBModel* model=nullptr;
-	arma::sp_mat M;		 arma::vec q;		 perps Compl;
-	// Game::LCP *MyNashGame = nullptr;
-	arma::sp_mat Aa; arma::vec b;
-	Models::EPEC epec(&env);
-	try
-	{
-		epec.addCountry(LA, 0).addCountry(LA2, 0).addTranspCosts(TrCo).finalize();
-		epec.make_country_QP();
-		try{epec.testQP(0);}catch(...){}
-		try{epec.testQP(1);}catch(...){}
-		epec.testCountry(1);
-		epec.testCountry(0);
-		epec.findNashEq(true);
-		cout<<"--------------------------------------------------Printing Locations--------------------------------------------------\n";
-		for(unsigned int i = 0; i<epec.nCountries; i++)
-		{
-			cout<<"********** Country number "<<i+1<<"\t\t"<<"**********\n";
-			for(int j=0; j<9;j++)
-			{
-				auto v = static_cast<Models::LeaderVars>(j);
-				cout<<Models::prn::label<<std::setfill('.')<<v<<Models::prn::val<<std::setfill('.')<<epec.getPosition(i, v)<<endl;
-			}
-			cout<<endl;
-		}
-		cout<<"--------------------------------------------------Printing Locations--------------------------------------------------\n";
-	} 
-	catch(const char* e) { cerr<<e<<endl;throw; }
-	catch(string e) { cerr<<"String: "<<e<<endl;throw; }
-	catch(exception &e) { cerr<<"Exception: "<<e.what()<<endl;throw; }
-	catch(GRBException &e) {cerr<<"GRBException: "<<e.getErrorCode()<<": "<<e.getMessage()<<endl;throw;}
-	return 0;
+int LCPtest(Models::LeadAllPar LA, Models::LeadAllPar LA2, arma::sp_mat TrCo) {
+    GRBEnv env = GRBEnv();
+    // GRBModel* model=nullptr;
+    arma::sp_mat M;
+    arma::vec q;
+    perps Compl;
+    // Game::LCP *MyNashGame = nullptr;
+    arma::sp_mat Aa;
+    arma::vec b;
+    Models::EPEC epec(&env);
+    try {
+        epec.addCountry(LA, 0).addCountry(LA2, 0).addTranspCosts(TrCo).finalize();
+        epec.make_country_QP();
+        try { epec.testQP(0); } catch (...) {}
+        try { epec.testQP(1); } catch (...) {}
+        epec.testCountry(1);
+        epec.testCountry(0);
+        epec.findNashEq(true);
+        cout
+                << "--------------------------------------------------Printing Locations--------------------------------------------------\n";
+        for (unsigned int i = 0; i < epec.nCountries; i++) {
+            cout << "********** Country number " << i + 1 << "\t\t" << "**********\n";
+            for (int j = 0; j < 9; j++) {
+                auto v = static_cast<Models::LeaderVars>(j);
+                cout << Models::prn::label << std::setfill('.') << v << Models::prn::val << std::setfill('.')
+                     << epec.getPosition(i, v) << endl;
+            }
+            cout << endl;
+        }
+        cout
+                << "--------------------------------------------------Printing Locations--------------------------------------------------\n";
+    }
+    catch (const char *e) {
+        cerr << e << endl;
+        throw;
+    }
+    catch (string e) {
+        cerr << "String: " << e << endl;
+        throw;
+    }
+    catch (exception &e) {
+        cerr << "Exception: " << e.what() << endl;
+        throw;
+    }
+    catch (GRBException &e) {
+        cerr << "GRBException: " << e.getErrorCode() << ": " << e.getMessage() << endl;
+        throw;
+    }
+    return 0;
 }
 
 
+int main() {
+    Models::DemPar P;
+    Models::FollPar FP, FP2, FP3, FP1, FP3a;
 
-int main()
-{
-	Models::DemPar P;
-	Models::FollPar FP, FP2, FP3, FP1, FP3a;
+    Models::LeadPar L(0.4, -1, -1, -1);
 
-	Models::LeadPar L (0.4,-1,-1,-1);
+    FP1.capacities = {1000};
+    FP1.costs_lin = {10};
+    FP1.costs_quad = {0.1};
+    FP1.emission_costs = {0};
 
-	FP1.capacities = {1000};
-	FP1.costs_lin = {10};
-	FP1.costs_quad = {0.1};
-	FP1.emission_costs = {0};
+    FP.capacities = {10, 15};
+    FP.costs_lin = {0, 4};
+    FP.costs_quad = {0, 40};
+    FP.emission_costs = {0, 0};
 
-	FP.capacities = {10, 15};
-	FP.costs_lin = {0, 4};
-	FP.costs_quad = {0, 40}; 
-	FP.emission_costs = {0, 0}; 
+    FP2.capacities = {10, 10};
+    FP2.costs_lin = {30, 50};
+    FP2.costs_quad = {20, 40};
+    FP2.emission_costs = {10, 0};
 
-	FP2.capacities = {10, 10};
-	FP2.costs_lin = {30, 50};
-	FP2.costs_quad = {20, 40}; 
-	FP2.emission_costs = {10, 0}; 
+    FP3.capacities = {10, 15, 50};
+    FP3.costs_lin = {30, 32, 5};
+    FP3.costs_quad = {60, 40, 10};
+    FP3.emission_costs = {0, 1, 10};
+    FP3.names = {"Solar producer", "Gas producer", "Coal producer"};
 
-	FP3.capacities = {10, 15, 50};
-	FP3.costs_lin = {30, 32, 5};
-	FP3.costs_quad = {60, 40, 10}; 
-	FP3.emission_costs = {0, 1, 10}; 
-	FP3.names = {"Solar producer", "Gas producer", "Coal producer"};
-
-	FP3a.capacities = {10, 15, 50};
-	FP3a.costs_lin = {30, 32, 5};
-	FP3a.costs_quad = {60, 40, 10}; 
-	FP3a.emission_costs = {0, 1, 10}; 
-	FP3a.names = {"Solar producer", "Gas producer", "Coal producer"};
+    FP3a.capacities = {10, 15, 50};
+    FP3a.costs_lin = {30, 32, 5};
+    FP3a.costs_quad = {60, 40, 10};
+    FP3a.emission_costs = {0, 1, 10};
+    FP3a.names = {"Solar producer", "Gas producer", "Coal producer"};
 /*
 */
 
-	// Two followers Leader with price cap
-	Models::LeadAllPar LA_pc1(3, "USA", FP3, {40,0.10}, {0.4, -1, -1, -1});
-	Models::LeadAllPar LA_pc2(3, "China", FP3, {60,0.25}, {0.4, -1, -1, -1});
+    // Two followers Leader with price cap
+    Models::LeadAllPar LA_pc1(3, "USA", FP3, {40, 0.10}, {0.4, -1, -1, -1});
+    Models::LeadAllPar LA_pc2(3, "China", FP3, {60, 0.25}, {0.4, -1, -1, -1});
 
-	// cout<<LA<<LA2;
-	// cout<<LA.FollowerParam.capacities.size()<<" "<<LA.FollowerParam.costs_lin.size()<<" "<<LA.FollowerParam.costs_quad.size()<<endl;
-	arma::mat TrCo(2,2); 
-	TrCo << 0 << 0.5<< arma::endr << 0.5 <<0;
-	LCPtest(LA_pc1, LA_pc2, arma::sp_mat(TrCo));
+    // cout<<LA<<LA2;
+    // cout<<LA.FollowerParam.capacities.size()<<" "<<LA.FollowerParam.costs_lin.size()<<" "<<LA.FollowerParam.costs_quad.size()<<endl;
+    arma::mat TrCo(2, 2);
+    TrCo << 0 << 0.5 << arma::endr << 0.5 << 0;
+    LCPtest(LA_pc1, LA_pc2, arma::sp_mat(TrCo));
 
 
-	return 0;
+    return 0;
 }
-
--- src/lcptolp.h
+++ src/lcptolp.h
@@ -1,20 +1,24 @@
 #ifndef LCPTOLP_H
 #define LCPTOLP_H
 
-#include"epecsolve.h"
-#include<iostream>
-#include<memory>
-#include<gurobi_c++.h>
-#include<armadillo>
+#include "epecsolve.h"
+#include <iostream>
+#include <memory>
+#include <gurobi_c++.h>
+#include <armadillo>
 
 //using namespace Game;
 
+namespace Game {
 
-namespace Game{
+    arma::vec
+    LPSolve(const arma::sp_mat &A, const arma::vec &b, const arma::vec &c, int &status, bool Positivity = false);
 
-arma::vec LPSolve(const arma::sp_mat &A, const arma::vec &b, const arma::vec &c, int &status, bool Positivity=false);
-int ConvexHull(const vector<arma::sp_mat*> *Ai, const vector<arma::vec*> *bi, arma::sp_mat &A, arma::vec &b, const arma::sp_mat Acom={}, const arma::vec bcom={});
-void compConvSize(arma::sp_mat &A, const unsigned int nFinCons, const unsigned int nFinVar, const vector<arma::sp_mat*> *Ai, 	const vector<arma::vec*> *bi 	);
+    int ConvexHull(const vector<arma::sp_mat *> *Ai, const vector<arma::vec *> *bi, arma::sp_mat &A, arma::vec &b,
+                   const arma::sp_mat Acom = {}, const arma::vec bcom = {});
+
+    void compConvSize(arma::sp_mat &A, const unsigned int nFinCons, const unsigned int nFinVar,
+                      const vector<arma::sp_mat *> *Ai, const vector<arma::vec *> *bi);
 /**
  * @brief Class to handle and solve linear complementarity problems
  */
@@ -23,102 +27,140 @@ void compConvSize(arma::sp_mat &A, const unsigned int nFinCons, const unsigned i
 * especially as MIPs with bigM constraints
 * Also provides the convex hull of the feasible space, restricted feasible space etc.
 */
-class LCP
-{
-	private:
-	// Essential data ironment for MIP/LP solves
-		GRBEnv* env;			///< Gurobi env 
-		arma::sp_mat M; 		///< M in @f$Mx+q@f$ that defines the LCP 
-		arma::vec q; 			///< q in @f$Mx+q@f$ that defines the LCP 
-		perps Compl; 			///< Compl stores data in <Eqn, Var> form.
-		unsigned int LeadStart, LeadEnd, nLeader; 
-		arma::sp_mat _A={}; arma::vec _b={};	///< Apart from @f$0 \le x \perp Mx+q\ge 0@f$, one needs@f$ Ax\le b@f$ too!
-	// Temporary data 
-		bool madeRlxdModel{false};	///< Keep track if LCP::RlxdModel is made
-		unsigned int nR, nC;
-		/// LCP feasible region is a union of polyhedra. Keeps track which of those inequalities are fixed to equality to get the individual polyhedra
-		vector<vector<short int>*> *AllPolyhedra, *RelAllPol;
-		vector<arma::sp_mat*> *Ai, *Rel_Ai; vector<arma::vec*> *bi, *Rel_bi; 
-	   	GRBModel RlxdModel;	///< A gurobi model with all complementarity constraints removed.
-	public: 
-	// Fudgible data 
-		long double bigM {1e5};	///< bigM used to rewrite the LCP as MIP 
-		long double eps {1e-5};	///< The threshold, below which a number would be considered to be zero.
-	public:
-	/** Constructors */
-		/// Class has no default constructors
-		LCP() = delete;	
-		LCP(GRBEnv* env, arma::sp_mat M, arma::vec q, 
-				unsigned int LeadStart, unsigned LeadEnd, arma::sp_mat A={}, arma::vec b={}); // Constructor with M,q,leader posn
-		LCP(GRBEnv* env, arma::sp_mat M, arma::vec q, 
-				perps Compl, arma::sp_mat A={}, arma::vec b={}); // Constructor with M, q, compl pairs
-		LCP(GRBEnv* env, const Game::NashGame &N);
-	/** Destructor - to delete the objects created with new operator */
-		~LCP();
-	/** Return data and address */ 
-		inline arma::sp_mat getM() {return this->M;}  			///< Read-only access to LCP::M 
-		inline arma::sp_mat* getMstar() {return &(this->M);}	///< Reference access to LCP::M 
-		inline arma::vec getq() {return this->q;}  				///< Read-only access to LCP::q 
-		inline arma::vec* getqstar() {return &(this->q);}		///< Reference access to LCP::q 
-		inline unsigned int getLStart(){return LeadStart;} 		///< Read-only access to LCP::LeadStart 
-		inline unsigned int getLEnd(){return LeadEnd;}			///< Read-only access to LCP::LeadEnd 
-		inline perps getCompl() {return this->Compl;}  			///< Read-only access to LCP::Compl 
-		void print(string end="\n");							///< Print a summary of the LCP
-		inline unsigned int getNcol() {return this->M.n_cols;};
-		inline unsigned int getNrow() {return this->M.n_rows;};
-	/* Member functions */
-	private:
-		bool errorCheck(bool throwErr=true) const;
-		void defConst(GRBEnv* env);
-		void makeRelaxed();
-	/* Solving relaxations and restrictions */
-	private:
-		unique_ptr<GRBModel> LCPasMIP(vector<unsigned int> FixEq={}, 
-				vector<unsigned int> FixVar={}, bool solve=false);
-		unique_ptr<GRBModel> LCPasMIP(vector<short int> Fixes, bool solve);
-		unique_ptr<GRBModel> LCP_Polyhed_fixed(vector<unsigned int> FixEq={}, 
-				vector<unsigned int> FixVar={});
-		unique_ptr<GRBModel> LCP_Polyhed_fixed(arma::Col<int> FixEq, 
-				arma::Col<int> FixVar);
-	/* Branch and Prune Methods */
-	private:
-		template<class T> inline bool isZero(const T val) const { return (val>-eps && val < eps);}
-		inline vector<short int>* solEncode(GRBModel *model)const;
-		vector<short int>* solEncode(const arma::vec &z, const arma::vec &x)const;
-		void branch(int loc, const vector<short int> *Fixes);
-		vector<short int>* anyBranch(const vector<vector<short int>*>* vecOfFixes, vector<short int>* Fix) const;
-		int branchLoc(unique_ptr<GRBModel> &m, vector<short int>* Fix);
-		int branchProcLoc(vector<short int>* Fix, vector<short int> *Leaf);
-		LCP& EnumerateAll(bool solveLP=false);
-	public:
-		bool extractSols(GRBModel* model, arma::vec &z, 
-				arma::vec &x, bool extractZ = false) const; 
-		vector<vector<short int>*> *BranchAndPrune ();
-	/* Getting single point solutions */
-	public:
-		unique_ptr<GRBModel> LCPasQP(bool solve = false);
-		unique_ptr<GRBModel> LCPasMIP(bool solve = false);
-		unique_ptr<GRBModel> MPECasMILP(const arma::sp_mat &C, const arma::vec &c, const arma::vec &x_minus_i, bool solve = false);
-		unique_ptr<GRBModel> MPECasMIQP(const arma::sp_mat &Q, const arma::sp_mat &C, const arma::vec &c, const arma::vec &x_minus_i, bool solve = false);
-	/* Convex hull computation */
-	private:
-		LCP& FixToPoly(const vector<short int> *Fix, bool checkFeas = false, bool custom=false, vector<arma::sp_mat*> *custAi={}, vector<arma::vec*> *custbi={});
-		LCP& FixToPolies(const vector<short int> *Fix, bool checkFeas = false, bool custom=false, vector<arma::sp_mat*> *custAi={}, vector<arma::vec*> *custbi={});
-	public:
-		LCP& addPolyhedron(const vector<short int> &Fix, vector<arma::sp_mat*> &custAi, vector<arma::vec*> &custbi, 
-				const bool convHull = false, arma::sp_mat *A={}, arma::vec  *b={});
-		int ConvexHull( 
-				arma::sp_mat& A,		///< Convex hull inequality description LHS to be stored here 
-			   	arma::vec &b) 			///< Convex hull inequality description RHS to be stored here
-		/**
-		 * Computes the convex hull of the feasible region of the LCP
-		 * @warning To be run only after LCP::BranchAndPrune is run. Otherwise this can give errors
-		 * @todo Formally call LCP::BranchAndPrune or throw an exception if this method is not already run
-		 */
-		{return Game::ConvexHull(this->Ai, this->bi, A, b, this->_A,this->_b);};
-		LCP& makeQP(const vector<short int> &Fix, vector<arma::sp_mat*> &custAi, vector<arma::vec*> &custbi, Game::QP_objective &QP_obj, Game::QP_Param &QP);
-		LCP& makeQP(Game::QP_objective &QP_obj, Game::QP_Param &QP);
-};
-};
-#endif
+    class LCP {
+    private:
+        // Essential data ironment for MIP/LP solves
+        GRBEnv *env;    ///< Gurobi env
+        arma::sp_mat M; ///< M in @f$Mx+q@f$ that defines the LCP
+        arma::vec q;    ///< q in @f$Mx+q@f$ that defines the LCP
+        perps Compl;    ///< Compl stores data in <Eqn, Var> form.
+        unsigned int LeadStart, LeadEnd, nLeader;
+        arma::sp_mat _A = {};
+        arma::vec _b = {};           ///< Apart from @f$0 \le x \perp Mx+q\ge 0@f$, one needs@f$ Ax\le b@f$ too!
+        // Temporary data
+        bool madeRlxdModel{false}; ///< Keep track if LCP::RlxdModel is made
+        unsigned int nR, nC;
+        /// LCP feasible region is a union of polyhedra. Keeps track which of those inequalities are fixed to equality to get the individual polyhedra
+        vector<vector<short int> *> *AllPolyhedra, *RelAllPol;
+        vector<arma::sp_mat *> *Ai, *Rel_Ai;
+        vector<arma::vec *> *bi, *Rel_bi;
+        GRBModel RlxdModel; ///< A gurobi model with all complementarity constraints removed.
 
+        bool errorCheck(bool throwErr = true) const;
+
+        void defConst(GRBEnv *env);
+
+        void makeRelaxed();
+
+        /* Solving relaxations and restrictions */
+        unique_ptr<GRBModel> LCPasMIP(vector<unsigned int> FixEq = {},
+                                      vector<unsigned int> FixVar = {}, bool solve = false);
+
+        unique_ptr<GRBModel> LCPasMIP(vector<short int> Fixes, bool solve);
+
+        unique_ptr<GRBModel> LCP_Polyhed_fixed(vector<unsigned int> FixEq = {},
+                                               vector<unsigned int> FixVar = {});
+
+        unique_ptr<GRBModel> LCP_Polyhed_fixed(arma::Col<int> FixEq,
+                                               arma::Col<int> FixVar);
+
+        /* Branch and Prune Methods */
+        template<class T>
+        inline bool isZero(const T val) const { return (val > -eps && val < eps); }
+
+        inline vector<short int> *solEncode(GRBModel *model) const;
+
+        vector<short int> *solEncode(const arma::vec &z, const arma::vec &x) const;
+
+        void branch(int loc, const vector<short int> *Fixes);
+
+        vector<short int> *anyBranch(const vector<vector<short int> *> *vecOfFixes, vector<short int> *Fix) const;
+
+        int branchLoc(unique_ptr<GRBModel> &m, vector<short int> *Fix);
+
+        int branchProcLoc(vector<short int> *Fix, vector<short int> *Leaf);
+
+        LCP &EnumerateAll(bool solveLP = false);
+
+        LCP &FixToPoly(const vector<short int> *Fix, bool checkFeas = false, bool custom = false,
+                       vector<arma::sp_mat *> *custAi = {}, vector<arma::vec *> *custbi = {});
+
+        LCP &FixToPolies(const vector<short int> *Fix, bool checkFeas = false, bool custom = false,
+                         vector<arma::sp_mat *> *custAi = {}, vector<arma::vec *> *custbi = {});
+
+    public:
+        // Fudgible data
+        long double bigM{1e7}; ///< bigM used to rewrite the LCP as MIP
+        long double eps{1e-8}; ///< The threshold, below which a number would be considered to be zero.
+
+        /** Constructors */
+        /// Class has no default constructors
+        LCP() = delete;
+
+        LCP(GRBEnv *env, arma::sp_mat M, arma::vec q,
+            unsigned int LeadStart, unsigned LeadEnd, arma::sp_mat A = {},
+            arma::vec b = {}); // Constructor with M,q,leader posn
+        LCP(GRBEnv *env, arma::sp_mat M, arma::vec q,
+            perps Compl, arma::sp_mat A = {}, arma::vec b = {}); // Constructor with M, q, compl pairs
+        LCP(GRBEnv *env, const Game::NashGame &N);
+
+        /** Destructor - to delete the objects created with new operator */
+        ~LCP();
+
+        /** Return data and address */
+        inline arma::sp_mat getM() { return this->M; }           ///< Read-only access to LCP::M
+        inline arma::sp_mat *getMstar() { return &(this->M); } ///< Reference access to LCP::M
+        inline arma::vec getq() { return this->q; }               ///< Read-only access to LCP::q
+        inline arma::vec *getqstar() { return &(this->q); }    ///< Reference access to LCP::q
+        inline unsigned int getLStart() { return LeadStart; }  ///< Read-only access to LCP::LeadStart
+        inline unsigned int getLEnd() { return LeadEnd; }      ///< Read-only access to LCP::LeadEnd
+        inline perps getCompl() { return this->Compl; }           ///< Read-only access to LCP::Compl
+        void print(string end = "\n");                           ///< Print a summary of the LCP
+        inline unsigned int getNcol() { return this->M.n_cols; };
+
+        inline unsigned int getNrow() { return this->M.n_rows; };
+
+
+        bool extractSols(GRBModel *model, arma::vec &z,
+                         arma::vec &x, bool extractZ = false) const;
+
+        vector<vector<short int> *> *BranchAndPrune();
+
+        /* Getting single point solutions */
+        unique_ptr<GRBModel> LCPasQP(bool solve = false);
+
+        unique_ptr<GRBModel> LCPasMIP(bool solve = false);
+
+        unique_ptr<GRBModel>
+        MPECasMILP(const arma::sp_mat &C, const arma::vec &c, const arma::vec &x_minus_i, bool solve = false);
+
+        unique_ptr<GRBModel>
+        MPECasMIQP(const arma::sp_mat &Q, const arma::sp_mat &C, const arma::vec &c, const arma::vec &x_minus_i,
+                   bool solve = false);
+
+
+        /* Convex hull computation */
+        LCP &addPolyhedron(const vector<short int> &Fix, vector<arma::sp_mat *> &custAi, vector<arma::vec *> &custbi,
+                           const bool convHull = false, arma::sp_mat *A = {}, arma::vec *b = {});
+
+        int ConvexHull(
+                arma::sp_mat &A, ///< Convex hull inequality description LHS to be stored here
+                arma::vec &b)    ///< Convex hull inequality description RHS to be stored here
+        /**
+             * Computes the convex hull of the feasible region of the LCP
+             * @warning To be run only after LCP::BranchAndPrune is run. Otherwise this can give errors
+             * @todo Formally call LCP::BranchAndPrune or throw an exception if this method is not already run
+             */
+        {
+            return Game::ConvexHull(this->Ai, this->bi, A, b, this->_A, this->_b);
+        };
+
+        LCP &makeQP(const vector<short int> &Fix, vector<arma::sp_mat *> &custAi, vector<arma::vec *> &custbi,
+                    Game::QP_objective &QP_obj, Game::QP_Param &QP);
+
+        LCP &makeQP(Game::QP_objective &QP_obj, Game::QP_Param &QP);
+
+        void write(string filename, bool append = true) const;
+    };
+}; // namespace Game
+#endif
