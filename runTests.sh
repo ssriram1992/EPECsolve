0a9da4899f599023a7b1261ad718193dac352b79
Gabriele Dragotto <gabriele@dragotto.net>
2019-10-01 11:34:06 -0400

    Improved runTests.sh

--- runTests.sh
+++ runTests.sh
@@ -1,12 +1,23 @@
 dataFolder="dat"
-resultsFolder=${dataFolder}/results
+instanceFolder=${dataFolder}/Instance_345_Harder
+resultsFolder=${dataFolder}/results_Instance_345_Harder
 logFile=${resultsFolder}/results.csv
-numThreads=12
+numThreads=8
 mkdir ${resultsFolder}
-for ex in $(ls ${dataFolder}/*.json); do
+for ex in $(ls ${instanceFolder}/*.json); do
       instanceName=${ex/.json/}
       instanceNumber=${ex//[!0-9]/}
       echo "----------------------Running instance $ex----------------------"
-      ./EPEC -i ${ex//.json/} -t ${numThreads} -w 2 -l ${logFile} -s ${resultsFolder}/Solution_${instanceNumber} --timelimit 3600
+      #Skip full enumeration with the first argument
+      if [ ! $1 ]; then
+        echo "--------Full Enumeration"
+        ./EPEC -i ${ex//.json/} -t ${numThreads} -w 2 -l ${logFile} -s ${resultsFolder}/Solution_Full_${instanceNumber} --timelimit 1800 -a 0
+      fi
+      for aggressiveness in {1,3,5}; do
+        for addPolyMethod in {0,1,2}; do
+          echo "--------Inner Approximation"
+          ./EPEC -i ${ex//.json/} -t ${numThreads} -w 2 -l ${logFile} -s ${resultsFolder}/Solution_Inner_${instanceNumber}_${aggressiveness}_${addPolyMethod} --timelimit 1800 -a 1 --aggr $aggressiveness --add $addPolyMethod
+        done
+      done
       printf "\n\n"
 done
